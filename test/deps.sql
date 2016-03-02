7c320f89f07052b3852888baacf7f024c4a8f06d
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-03-01 19:51:04 -0600

    Need to install tap first

--- test/deps.sql
+++ test/deps.sql
@@ -1,4 +1,9 @@
 -- Note: pgTap is loaded by setup.sql
+-- ... But we need to manually install it for tg_sanity_tap;
+
+CREATE SCHEMA IF NOT EXISTS tap;
+CREATE EXTENSION IF NOT EXISTS pgtap SCHEMA tap;
+
 CREATE EXTENSION tg_sanity;
 CREATE EXTENSION tg_sanity_tap;
 
